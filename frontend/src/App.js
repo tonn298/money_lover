539fe8ca4ea00277b00650d4af29fe04714d683f
tonn298 <c.chirdkiatisak@gmail.com>
2021-06-21 16:01:52 +0700

    add router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,18 @@
+import Home from "./pages/home";
+import styled from "styled-components";
+
+const AppStyled = styled.div`
+  /* TODOS make this a desktop view later */
+  background-color: #111111;
+  padding: 12px;
+`;
+
 const App = () => {
-  return <div>hello</div>;
+  return (
+    <AppStyled>
+      <Home></Home>
+    </AppStyled>
+  );
 };
 
 export default App;
---
+++ frontend/src/pages/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const HomeStyled = styled.div`
+  background-color: #aaaaaa;
+`;
+
+const Home = () => {
+  return <HomeStyled>yo</HomeStyled>;
+};
+
+export default Home;
